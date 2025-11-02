65592d2224f5d0c352beba04fe175c93aa0daae2
Alisson Yagi <alissonyagi@gmail.com>
2025-11-02 14:13:48 -0300

    Database transaction isolation with timeout support

--- Database.js
+++ Database.js
@@ -8,12 +8,88 @@ const str = new Strings('main/Database')
 
 const defaultDatabase = path.resolve(config.project.path || require.main.path, 'db', 'db.sqlite')
 
+class DatabaseTransaction {
+	_db
+	_timeout
+
+	constructor (db, timeout) {
+		if (!(db instanceof Database))
+			throw str.error('invalid-transaction-database', null, { db: db })
+
+		if (isNaN(timeout) || parseInt(timeout) <= 0)
+			throw str.error('invalid-transaction-timeout', null, { timeout: timeout })
+
+		this._db = new Database(db._filename, db._opts)
+
+		if (!this._db.isOpen)
+			this._db.open()
+
+		this._timeout = setTimeout(function () {
+			this.rollback()
+			throw str.error('transaction-timeout')
+		}.bind(this), timeout)
+
+		this._db.exec('BEGIN TRANSACTION')
+	}
+
+	commit () {
+		try {
+			clearTimeout(this._timeout)
+
+			let ret = this._db.exec('COMMIT')
+
+			this._db.close()
+
+			return ret
+		}
+		catch (e) {
+			throw str.error('commit-failed', null, { error: e })
+		}
+	}
+
+	rollback () {
+		try {
+			clearTimeout(this._timeout)
+
+			let ret = this._db.exec('ROLLBACK')
+
+			this._db.close()
+
+			return ret
+		}
+		catch (e) {
+			throw str.error('rollback-failed', null, { error: e })
+		}
+	}
+
+	run (...args) {
+		return this._db.run(...args)
+	}
+
+	get (...args) {
+		return this._db.get(...args)
+	}
+
+	all (...args) {
+		return this._db.all(...args)
+	}
+}
+
 module.exports = class Database extends sqlite.DatabaseSync {
+	_filename
+	_opts
+
 	constructor (filename = defaultDatabase, opts = {}) {
 		try {
 			super(filename, opts)
+
+			this._filename = filename
+			this._opts = JSON.parse(JSON.stringify(opts))
 		}
 		catch (e) {
+			if (this.isOpen)
+				this.close()
+
 			throw str.error('load-failed', null, { filename: filename })
 		}
 
@@ -50,30 +126,12 @@ module.exports = class Database extends sqlite.DatabaseSync {
 		}
 	}
 
-	begin () {
+	begin (timeout = 500) {
 		try {
-			return this.exec('BEGIN TRANSACTION')
+			return new DatabaseTransaction(this, timeout)
 		}
 		catch (e) {
 			throw str.error('begin-transaction-failed', null, { error: e })
-		}
-	}
-
-	commit () {
-		try {
-			return this.exec('COMMIT')
-		}
-		catch (e) {
-			throw str.error('commit-failed', null, { error: e })
-		}
-	}
-
-	rollback () {
-		try {
-			return this.exec('ROLLBACK')
-		}
-		catch (e) {
-			throw str.error('rollback-failed', null, { error: e })
 		}
 	}
 
